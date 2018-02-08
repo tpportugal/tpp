6a1379af84f7c6014f27cddfd97c94ebc50c386b
André Glatzl <andre@glatzl.me>
2018-02-08 18:19:55 +0100

    Set www.tpp.pt on allowed_hosts

--- api/settings.py
+++ api/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'v1)g@8)7t&vj#q24yx8d+m3&v&emz&6u9f9g9mol96j8i7q_z0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'tpp.pt']
+ALLOWED_HOSTS = ['0.0.0.0', 'tpp.pt', 'www.tpp.pt']
 
 # Application definition
 
