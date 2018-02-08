6e17416817f2509febb72359e73df6ecf1df73e0
André Glatzl <andre@glatzl.me>
2018-02-08 18:09:12 +0100

    Set static root

--- api/settings.py
+++ api/settings.py
@@ -111,6 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
