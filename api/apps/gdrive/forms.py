f53e956c15cd4fd8643fa031426bd4860d2f4f7e
André Glatzl <andre@glatzl.me>
2018-02-09 01:18:43 +0100

    Only allow specific files

--- api/apps/gdrive/forms.py
+++ api/apps/gdrive/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, FileInput
 
 from api.apps.companies.models import Company
 from api.apps.gdrive.models import Timetable
@@ -7,6 +7,7 @@ from api.apps.gdrive.models import Timetable
 
 class TimetableForm(forms.ModelForm):
     company = ModelChoiceField(queryset=Company.objects.all(), to_field_name='slug', label="Operadora de transportes")
+    map_data = forms.FileField(widget=FileInput(attrs={'accept':'application/pdf,image/jpeg,image/png'}))
 
     class Meta:
         model = Timetable
--- api/apps/gdrive/models.py
+++ api/apps/gdrive/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.forms import FileInput
 
 from api.apps.companies.models import Company
 
@@ -7,6 +6,6 @@ from api.apps.companies.models import Company
 class Timetable(models.Model):
     id = models.AutoField(primary_key=True)
     map_name = models.CharField(max_length=200)
-    map_data = models.FileField(upload_to='maps', verbose_name="Ficheiro", widget=FileInput(attrs={'accept':'application/pdf,image/jpeg,image/png'}))
+    map_data = models.FileField(upload_to='maps', verbose_name="Ficheiro")
     # Training Data
     company = models.ForeignKey(Company, models.DO_NOTHING)
