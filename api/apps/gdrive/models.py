530e75dacd6f2b094b2ff705de0cec8039c838a7
André Glatzl <andre@glatzl.me>
2018-02-09 01:14:22 +0100

    Remove access

--- api/apps/gdrive/models.py
+++ api/apps/gdrive/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.forms import FileInput
+
 from api.apps.companies.models import Company
 
 
 class Timetable(models.Model):
     id = models.AutoField(primary_key=True)
     map_name = models.CharField(max_length=200)
-    map_data = models.FileField(upload_to='maps', verbose_name="Ficheiro")
+    map_data = models.FileField(upload_to='maps', verbose_name="Ficheiro", widget=FileInput(attrs={'accept':'application/pdf,image/jpeg,image/png'}))
     # Training Data
     company = models.ForeignKey(Company, models.DO_NOTHING)
