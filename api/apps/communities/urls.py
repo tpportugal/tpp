ac4ea0e436cf1432c81578a88ab0bdadb81bc87b
André Glatzl <andre@glatzl.me>
2018-02-12 22:32:28 +0100

    Removal of model: Community

--- api/apps/communities/urls.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import routers
-
-from api.apps.communities.views import CommunityViewSet
-
-router = routers.DefaultRouter()
-router.register(r'communities', CommunityViewSet)
--- api/apps/communities/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-
-from api.apps.communities.models import Community
-from api.apps.communities.serializer import CommunitySerializer
-
-
-class CommunityViewSet(viewsets.ModelViewSet):
-    serializer_class = CommunitySerializer
-    queryset = Community.objects.all()
