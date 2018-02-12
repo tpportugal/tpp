9d08aee8d84b130f53c85ed61d2e3e5088c67ccb
André Glatzl <andre@glatzl.me>
2018-02-12 22:57:54 +0100

    Adding django debug toolbar # 2

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include
 from graphene_django.views import GraphQLView
 from api.apps.gdrive.views import upload_timetable
 
@@ -10,3 +11,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^upload/', upload_timetable),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
