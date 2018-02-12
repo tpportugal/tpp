687296eb2cbe7bd6f7eac3923f415bc9cbdb338d
André Glatzl <andre@glatzl.me>
2018-02-12 22:53:10 +0100

    Adding django debug toolbar

--- api/settings_development.py
+++ api/settings_development.py
@@ -4,17 +4,17 @@ DEBUG = True
 GRAPHQL_EDITOR = True
 ADMINS = []
 ALLOWED_HOSTS = ['0.0.0.0']
-#INSTALLED_APPS.append('debug_toolbar')
-#MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
-#DEBUG_TOOLBAR_PATCH_SETTINGS = False
-#
-#
-#def custom_show_toolbar(request):
-#    return True  # Always show toolbar, for example purposes only.
-#
-#
-#DEBUG_TOOLBAR_CONFIG = {
-#    'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
-#}
+INSTALLED_APPS.append('debug_toolbar')
+MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+
+def custom_show_toolbar(request):
+    return True  # Always show toolbar, for example purposes only.
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
+}
 
 STATIC_ROOT = None
