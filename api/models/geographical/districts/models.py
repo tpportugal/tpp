ed33c1eb4e5a8f367008830ad5d195c25461b58a
André Glatzl <andre@glatzl.me>
2018-02-16 00:15:30 +0100

    Update Module: District

--- api/models/geographical/districts/models.py
+++ api/models/geographical/districts/models.py
@@ -3,5 +3,8 @@ from django.db.models import SlugField, CharField
 
 
 class District(models.Model):
+    """
+        Modelo para os distritos em Portugal
+    """
     slug = SlugField()
     name = CharField(max_length=50)
